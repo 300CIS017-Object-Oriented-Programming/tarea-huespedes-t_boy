a8df580881ec65efb9eea09ac3c90bd9bfeb09be
Julio-Alejandro-Mazo <juliomazo10@javerianacali.edu.co>
2022-09-18 02:49:45 -0500

    NEW: se le agregaron nuevas clases como la de Reservas, la de Evaluacion y se termino de agregar los metodos de la clase Persona

---
+++ Reserva.cpp
@@ -0,0 +1,3 @@
+//
+// Created by julio on 18/09/2022.
+//
---
+++ Reserva.h
@@ -0,0 +1,8 @@
+//
+// Created by julio on 18/09/2022.
+//
+
+#ifndef TAREA_HUESPEDES_T_BOY_RESERVA_H
+#define TAREA_HUESPEDES_T_BOY_RESERVA_H
+
+#endif //TAREA_HUESPEDES_T_BOY_RESERVA_H
