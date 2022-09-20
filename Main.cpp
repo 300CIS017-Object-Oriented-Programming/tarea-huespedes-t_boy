d1354ff555a923ee8640290a728144dde5616044
Andres <andresfeliped078@gmail.com>
2022-09-19 21:30:16 -0500

    NEW: Se creo el archivo Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,34 @@
+#include "Sistema.h"
+
+int main(){
+    auto *sistema = new Sistema();
+
+    int arn;
+    do {
+        cout << "BIENVENIDOS T-BOY ALOJAMIENTOS" << endl;
+        cout << "******************************\n" << endl;
+
+        cout << "1) Cambiar la onformacion del Hogar\n" << endl;
+        cout << "2) Crear una reserva\n" << endl;
+        cout << "3) Eliminar reserva\n" << endl;
+        cout << "4) Añadir evalcuacion\n" << endl;
+        cout << "-1) Salir\n" << endl;
+        cout << "Elija una opcion: ";
+        cin >> arn;
+
+        switch (arn) {
+            case 1:
+                sistema->crearReservas();
+                break;
+            case 2:
+                sistema->liberarReservas();
+                break;
+            case 3:
+                sistema->agregarEvaluaciones();
+                break;
+            default:
+                break;
+        }
+    } while (arn != -1);
+    return 0;
+}
