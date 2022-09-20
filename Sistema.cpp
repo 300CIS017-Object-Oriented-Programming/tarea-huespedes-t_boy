979a05da68bafdee5b13701a43fc4dc66e09e58b
Andres <andresfeliped078@gmail.com>
2022-09-20 08:33:56 -0500

    MOD: Se añadieron las diferentes clases y se modificaron partes del codigo

--- Sistema.cpp
+++ Sistema.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include "Sistema.h"
+#include "Reserva.h"
 
 Sistema::Sistema(){
     cout << "Inicializa datos aqui\n";
@@ -12,18 +13,80 @@ void Sistema::agregarInformacionPropietario() {
     return true;
 }
 void Sistema::crearReservas() {
-    double idProp;
+    contReservas = 0
+    double identificacion;
     cout << "Escribe la Identificacion del propietario: ";
     cin >> idProp;
-    if (!Propietario(idProp)){
+    if (!Propietario(identificacion)){
         cout << "No hay informacion del propietario.\n" << endl;
         return;
     }
-    if (!Propietario(idProp).setDisponibilidad(false)){
-        cout << "No puede recibir huespedes"
+    if (!Propietario(identificacion).setDisponibilidad(false)){
+        cout << "No puede recibir huespedes" << endl;
     }
+    Propietario.(identificacion)->setDisponibilidad(true);
+
+    string fechaDeInicio, fechaDeFin;
+    cout << "Ingrese la fecha de incio de su hospedaje: ";
+    cin >> fechaDeInicio;
+    cout << "Ingrese la fecha de salida de su hospedaje: ";
+    cin >> fechaDeFin;
+    *new Reserva = new Reserva();
+    *new Reserva->getFechaDeInicio(fechaDeInicio);
+    *new Reserva->getFechaDeFin(fechaDeFin);
+    *new Reserva->setPropietario(Propietario(identificacion));
+    *new Reserva->setHuesped(Huesped(identificacion));
+
+    Reserva[contReservas] = new Reserva;
+    contReservas += 1;
+
+    cout << "Su reserva ha sido creada" << endl;
+
 }
 void Sistema::liberarReservas() {
+    int id;
+    cout << "Digite id de reserva: ";
+    cin >> id;
 
+    if (!Reserva(id)){
+        cout << "No existe reserva con ese numero" << endl;
+        return;
+    }
+    Reserva(id).getpropietario()->setDisponibilidad(true);
+}
+
+void Sistema::agregarEvaluaciones() {
+    double *emisor, *receptor;
+    contEval = 0
+    cout << "Ingrese su ID: ";
+    cin >> *emisor;
+    if (!Huesped(*emisor)){
+        cout << "No se encontro el usuario" << endl;
+        return;
+    }
+    cout << "Digite el ID de la persona a evaluar";
+    cin >> *receptor;
+    if (!Propietario(*receptor)){
+        cout << "No se encontro el usuario" << endl;
+        return;
+    }
+    string fechaEval, comentarios;
+    int calificacion;
+    cout << "Digite la Fecha en que realizo la calificacion: ";
+    cin >> fechaEval;
+    cout << "Digite la nota correspondiente: ";
+    cin >> calificacion;
+    cout << "Por favor deje un comentario: ";
+    cin >> comentarios;
+
+    *new Evaluacion = new Evaluacion();
+
+    *new Evaluacion->getCalificacion(calificacion);
+    *new Evaluacion->getComentarios(comentarios);
+    *new Evaluacion->getEmisor(*emisor);
+    *new Evaluacion->getReceptor(*receptor);
+
+    Evaluacion[contEval] = new Evaluacion;
+    contEval += 1;
 }
 
